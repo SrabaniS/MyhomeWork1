51f8d9057de494457387fded4357ec98cabfcfca
SrabaniS <srabani.suprakash@gmail>
2019-03-03 12:17:00 +0000

    MY First Change

---
+++ src/love/programming/MyHomeWorkMainClass.java
@@ -0,0 +1,19 @@
+package love.programming;
+
+public class MyHomeWorkMainClass {
+    public static void main(String[] args) {
+        //create 1st object for class MyHomeWorkClass1
+        MyHomeWorkClass1 obj1  = new MyHomeWorkClass1();
+
+        //create 2nd object for class MyHomeWorkClass1
+        MyHomeWorkClass1 obj2  = new MyHomeWorkClass1();
+
+        //create 1st object for class MyHomeWorkClass2
+        MyHomeWorkClass2 obj3  = new MyHomeWorkClass2();
+
+        //create 2nd object for class MyHomeWorkClass2
+        MyHomeWorkClass2 obj4  = new MyHomeWorkClass2();
+
+
+    }
+}
